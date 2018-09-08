084f93b01e35619e64a26be5147055e3f503fbd3
Oksana <o.shkoropad@gmail.com>
2018-09-08 18:36:12 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
+function myFunction(clickedId) {
+    document.calc.result.value+=clickedId;
+}
+function Clear() {
+    document.calc.result.value="";
+}
+function compute() {
+    try{
+    var inp=eval(document.calc.result.value);
+    document.calc.result.value=inp;
+    }catch(err){
+            document.calc.result.value="Bad Input!!";
+    }
 
 
 
