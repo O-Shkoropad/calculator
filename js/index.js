304f1b94e126e2fd71ba28c61250f33f8312373e
Oksana <o.shkoropad@gmail.com>
2018-09-09 20:48:19 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function myFunction(clickedId) {
-    document.calc.result.value+=clickedId;
-}
+    document.calc.result.value= document.calc.result.value +clickedId;
+ }
 function Clear() {
     document.calc.result.value="";
 }
@@ -9,6 +9,6 @@ function compute() {
     var inp=eval(document.calc.result.value);
     document.calc.result.value=inp;
     }catch(err){
-            document.calc.result.value="Bad Input!!";
+           // document.calc.result.value="Bad Input!!";
     }
 }
